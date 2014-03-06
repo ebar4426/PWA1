2ec777edfecfb235cf1079c109a97c92aa942968
ebar4426 <ebar4426@fullsail.edu>
2014-03-06 13:48:13 -0600

    following along

--- Wk1/goal1_assign_duel/js/script.js
+++ Wk1/goal1_assign_duel/js/script.js
@@ -4,4 +4,15 @@
 //assignment 1
 //part one
 
-//start function loop
+//start function
+(function(){
+
+    console.log("FIGHT!!!");
+
+    //player name
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
