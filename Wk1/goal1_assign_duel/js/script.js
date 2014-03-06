aed9eaa7cec612527cf24e753601ce05faaecaa4
ebar4426 <ebar4426@fullsail.edu>
2014-03-06 14:00:55 -0600

    added more code\

--- Wk1/goal1_assign_duel/js/script.js
+++ Wk1/goal1_assign_duel/js/script.js
@@ -14,3 +14,19 @@
   
     var player1Damage = 20;//player health
     var player2Damage = 20;//player 2 health
+    
+	var playerOneHealth = 100;//player one health
+    var playerTwoHealth = 100;//player two health
+    
+	var round = 0;//initiate round
+	
+	function fight(){
+		alert(playerOneName + ":" + playerOneHealth + " *START* " + playerTwoName + ":" + playerTwoHealth);
+		for (var i = 0; i < 10; i++)
+		{
+			//random form is = Math.floor(math.random() * (max - min) + min)
+			var minDamage1 = player1Damage * .5;
+			var minDamage2 = player2Damage * .5;
+			var f1 = Math.floor(Math.random() * (player1Damage - minDamage1) + minDamage1);
+			var f2 = Math.floor(Math.random() * (player2Damage - minDamage2) + minDamage2);
+	}
