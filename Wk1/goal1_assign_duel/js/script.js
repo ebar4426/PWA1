b5d183c956b64a35e57d378078b9fe76668b855e
ebar4426 <ebar4426@fullsail.edu>
2014-03-06 14:15:48 -0600

    finished with first stage in project

--- Wk1/goal1_assign_duel/js/script.js
+++ Wk1/goal1_assign_duel/js/script.js
@@ -24,9 +24,43 @@
 		alert(playerOneName + ":" + playerOneHealth + " *START* " + playerTwoName + ":" + playerTwoHealth);
 		for (var i = 0; i < 10; i++)
 		{
-			//random form is = Math.floor(math.random() * (max - min) + min)
+			//random formula is = Math.floor(math.random() * (max - min) + min)
 			var minDamage1 = player1Damage * .5;
 			var minDamage2 = player2Damage * .5;
 			var f1 = Math.floor(Math.random() * (player1Damage - minDamage1) + minDamage1);
 			var f2 = Math.floor(Math.random() * (player2Damage - minDamage2) + minDamage2);
-	}
+			
+			playerOneHealth -= f1;//how damage gets taken away
+            playerTwoHealth -= f2;//how damage gets taken away
+            
+            console.log(playerOneName + ": " + playerOneHealth + " " + playerTwoName + ":" + playerTwoHealth);
+            
+            var result = winnerCheck();//check for a winner if none code continues 
+            console.log(result);
+            if (result==="no winner")
+            {
+                round++;
+                alert(playerOneName+":"+playerOneHealth+"  *ROUND "+round+" OVER"+"*  "+playerTwoName+":"+playerTwoHealth);
+
+            } else{
+                alert(result);
+                break;
+            };
+
+          };
+    };
+
+    function winnerCheck(){
+        var result = "no winner";
+        if (playerOneHealth < 1 && playerTwoHealth < 1){
+            result = "You Both Die";
+        } else if(playerOneHealth < 1){
+            result = playerTwoName + " WINS!!!";    
+        } else if (playerTwoHealth < 1){
+            result = playerOneName + " WINS!!!";
+        };
+       return result;
+    };
+    //program starts below here_____________________________
+	fight();
+})();
